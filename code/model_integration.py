a1bc487df944430bfa77d8625eeb11616db0c4d8
Pablo Lechon <plechon@ucm.es>
2020-04-26 18:51:39 +0200

    Avoid concentrations go to 0 and clean-up

--- code/model_integration.py
+++ code/model_integration.py
@@ -20,34 +20,95 @@ global T; T = 298 # K
 
 ## FUNCTIONS ##
 
+def get_reac_network(s, m,  nATP, mu):
+    '''Create reaction network of feasible reactions (those which associated
+    Gibbs energy change is lower than nATP*D(G_ATP)) based on the
+    number of metabolites and microbial strains'''
+
+    #Prealocate the reaction matrix
+    reactions = np.zeros(shape = (s,m,m))
+    #Initialize reaction network container
+    reac_network = np.array([[], []], dtype = 'int32')
+    #Prealocate reaction network list where reaction networks for all strains
+    #will be stored
+    tot_reac_network = s*[reac_network]
+    #Create one reaction network for each microbial strain
+    for i in range(s):
+        #List of present metabolites from which substrates for reaction network
+        #will be drown
+        list_m_i = [0] 
+        keep_sampling = True
+        while keep_sampling: 
+            #Sample one reaction from all the posibilities
+            #Choose a substrate from list of present substrates (at the start
+            #only substrate 0 is present, so the network will always start at
+            #0)
+            m_i = int(np.random.choice(list_m_i))
+            #Create list of possible products of the reaction compatible where
+            #Delta G < 0
+            products = np.arange(m_i+1, m, dtype = int)
+            #Note that we start from m_i to satisfy the latter condition. 
+            #doing m_i + 1 avoids having reactions i-->i
+            #Choose a product from list of metabolites
+            m_j = np.random.choice(products)
+            #Create the tuple representing the sampled reaction
+            r_sample = np.array([[m_i], [m_j]])
+            #Is it an energetically valid reaction?
+            #Check if Gibbs energy change is not too big in order to assure 
+            #that the end metabolite is reached through multiple steps.
+            if mu[r_sample[1]] - mu[r_sample[0]] > -nATP*DeltaGATP:
+                #Add the reaction to reac_network
+                reac_network = np.concatenate((reac_network, r_sample),axis = 1)
+                #Eliminate repeated reactions
+                reac_network = np.unique(reac_network, axis = 1)
+                #Add product to list of substrates
+                list_m_i.append(m_j)
+                #When the last metabolite is reached, stop sampling. The 
+                #reaction network has reached the end product (that with the 
+                #lowest chemical potential)
+                if m-1 in reac_network[1]:
+                    keep_sampling = False
+        
+        #Transform to indices in reactions matrix
+        reac_network = tuple(reac_network)
+        #Switch on the matrix elements in reactions contained in reac_network
+        tot_reac_network[i] = reac_network
+        reactions[i][reac_network] = 1
+
+    return(reactions)
+
 def main(argv):
     '''Main function'''
 
     #Number of timepoints
-    n = 500
+    n = 100
     #Timepoints
-    t = np.linspace(1, 20, n)
+    t = np.linspace(1, 30, n)
     #Number of metabolites
-    m = 10
+    m = 50
     #Chemical potentials 
-    interval = 3e6 
+    #Total energy contained in metabolitess
+    interval = 3e6
+    #Sort decreasingly 
     mu_rev = np.sort(np.random.uniform(interval, size = m))
     mu = mu_rev[::-1]
     #Fix first and last chemical potentials
     mu[0] = interval 
     mu[-1] = 0
-    #Number of posible reactions
+    nATP = 10
+    #Number of posible reactions given thermodynamic constraint of only
+    #forward net reactions taking place
     n_reac = comb(m, 2, exact = True)
     #Number of strains
-    s = 2
+    s = 10
     #Initial conditions
+    #All metabolite concentrations equal 0, and all strains have 1 individual
     Cinit = [0]*m
-    Cinit[0] = 1
-    Ninit = [1]
-    #Prealocate matrices for solutions
+    Ninit = [1]*s
+    #Prealocate matrices for storing solution's time evolution
     C = np.zeros(shape = (m, len(t)))
     N = np.zeros(shape = (s, len(t)))
-    #Add initial conditions to the first element of the solution
+    #Add initial conditions to the first column of the solution's time series
     C[:,0] = Cinit
     N[:,0] = Ninit
     
@@ -72,53 +133,7 @@ def main(argv):
 
     #2. Choose reactions matrix for each strain
     ###########################################
-    reactions = np.zeros(shape = (s,m,m))
-    #Initialize reaction network container
-    reac_network = np.array([[], []], dtype = 'int32')
-    #Prealocate reaction network list where reaction networks for all strains
-    #will be stored
-    tot_reac_network = s*[reac_network]
-    for i in range(s):
-        #List of present metabolites from which substrates for reaction network
-        #will be drown
-        list_m_i = [0] 
-        keep_sampling = True
-        while keep_sampling: 
-            #Sample one reaction from all the posibilities
-            #Choose a substrate from list of present substrates (at the start
-            #only substrate 0 is present, so the network will always start at
-            #0)
-            m_i = int(np.random.choice(list_m_i))
-            #Create list of possible products of the reaction compatible where
-            #Delta G < 0
-            products = np.arange(m_i+1, m, dtype = int)
-            #Note that we start from m_i to satisfy the latter condition. 
-            #doing m_i + 1 avoids having reactions i-->i
-            #Choose a product from list of metabolites
-            m_j = np.random.choice(products)
-            #Create the tuple representing the sampled reaction
-            r_sample = np.array([[m_i], [m_j]])
-            #Is it an energetically valid reaction?
-            #Check if Gibbs energy change is not too big in order to assure 
-            #that the end metabolite is reached through multiple steps.
-            if mu[r_sample[1]] - mu[r_sample[0]] > -20*DeltaGATP:
-                #Add the reaction to reac_network
-                reac_network = np.concatenate((reac_network, r_sample),axis = 1)
-                #Eliminate repeated reactions
-                reac_network = np.unique(reac_network, axis = 1)
-                #Add product to list of substrates
-                list_m_i.append(m_j)
-                #When the last metabolite is reached, stop sampling. The 
-                #reaction network has reached the end product (that with the 
-                #lowest chemical potential)
-                if m-1 in reac_network[1]:
-                    keep_sampling = False
-        
-        #Transform to indices in reactions matrix
-        reac_network = tuple(reac_network)
-        #Switch on the matrix elements in reactions contained in reac_network
-        tot_reac_network[i] = reac_network
-        reactions[i][reac_network] = 1
+    reactions = get_reac_network(s, m, nATP, mu)
 
     #Numerically integrate the model
     for i in range(1, n):
@@ -151,7 +166,7 @@ def main(argv):
             Q = r_quotient(S, P)
             #Calculate Gibbs free energy changes
             mu_S = mu[tot_reac_network[j][0]]
-            mu_P = mu[reac_network[j][1]]
+            mu_P = mu[tot_reac_network[j][1]]
             #Gibs energy change is the difference between chemical potentials 
             #(stechiometric coefficients are all 1)
             DeltaG = mu_P - mu_S
@@ -190,11 +205,16 @@ def main(argv):
         #Store solutions 
         N[:,i] = z[1][0:s]
         C[:,i] = z[1][s:s+m]
+        #Transform negative elements to 0
+        C[:,i] = C[:,i].clip(min=0)
         #Next initial condition
         z0 = z[1]
     
     for i in range(len(N)):
         plt.plot(t, N[i])
+
+    for i in range(len(C)):
+        plt.plot(t, C[i], linestyle = '--')
         
     plt.show()
 
