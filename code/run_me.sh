2b06ee0b69828b975068353118236d11f452ec12
Pablo Lechon <plechon@ucm.es>
2020-08-28 11:18:20 +0200

    .

--- code/run_me.sh
+++ code/run_me.sh
@@ -12,3 +12,8 @@ python3 s_plot.py
 echo -e "\n Analysis of coalesced communities"
 Rscript analysis_community_coalescence.r 
 
+echo -e "\nGENERATING REPORT"
+cd writeup
+#Run latex document with the write up
+bash ~/CompileLaTeXstudio.sh Lechon_Pablo_CMEE_MSc_2020 1>../Sandbox/warnings.txt
+cd ..
